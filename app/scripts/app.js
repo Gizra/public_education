6e50e499d1b9b50f63d0ca7c7b8679f27ced4f4f
Amitaibu <amitai@gizra.com>
2013-09-10 15:04:03 +0300

    yo angular.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('publicEducationApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
