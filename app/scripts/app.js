58f2928eb42c33dd1492e5fcc5dd542df9d9f820
Amitai Burstein <amitai@gizra.com>
2013-09-16 21:20:25 +0300

    Add leaflet directive.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('publicEducationApp', [])
+angular.module('publicEducationApp', ["leaflet-directive"])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,13 @@
 
 angular.module('publicEducationApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    angular.extend($scope, {
+      center: {
+        lat: 40.095,
+        lng: -3.823,
+        zoom: 4
+      }
+    });
+
   });
