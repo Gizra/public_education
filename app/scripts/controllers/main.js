545559fc0c8d9b9b09bee2d984a87c5eb75026ee
Amitai Burstein <amitai@gizra.com>
2013-09-16 21:40:10 +0300

    Add markers.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,11 +4,26 @@ angular.module('publicEducationApp')
   .controller('MainCtrl', function ($scope) {
 
     angular.extend($scope, {
-      center: {
-        lat: 40.095,
-        lng: -3.823,
-        zoom: 4
+      istanbul: {
+        lat: 41.0383,
+        lng: 28.9869,
+        zoom: 20
+      },
+      markers: {
+        marker1: {
+          lat: 41.0383,
+          lng: 28.9869,
+          message: "This is Gezi Parki",
+          focus: false,
+          draggable: true
+        },
+        marker2: {
+          lat: 41.0383,
+          lng: 28.96,
+          message: "This is Barcelona. You can't drag me",
+          focus: false,
+          draggable: false
+        }
       }
     });
-
   });
