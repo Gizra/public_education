21c6c22ad4dc32d9f9c0c02d749f76aa64daf14b
Amitai Burstein <amitai@gizra.com>
2013-09-18 22:26:57 +0300

    Remove files.

--- www/scripts/services/records.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('publicEducationApp')
-  .service('Records', function Records() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
