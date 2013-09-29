633be31b8c00f1b85d00f211a36108741ac6e847
Amitai Burstein <amitai@gizra.com>
2013-09-29 21:12:42 +0300

    Updates

--- scripts/22f748c4.config.js
+++ scripts/22f748c4.config.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('config', []).constant('ENV', 'production').constant('FOURDQUARE', {
-  'id': 'client_id',
-  'secret': 'client_secret'
+  'id': 'IZPZNXG2P5A3H4EBIFFEMRAIBMXCNGM1ZZ2QUPN2D1WNKNGX',
+  'secret': 'MZ0CZ4Z0PMAEAGKDPJ5DO0BI2M1FAMJXRGWMCWNCPC5MQ02U'
 }).constant('BACKEND_URL', '');
 ;
