d1ad0a2be78d3fc8830045198f944822954af8a2
Carlos Mantilla <carlos@gizra.com>
2013-10-06 17:26:06 +0300

    - Modified view login - Add login view html add-marker view

--- app/scripts/controllers/addMarker.js
+++ app/scripts/controllers/addMarker.js
@@ -62,9 +62,21 @@ angular.module('publicEducationApp')
      *   - form:
      *   - record:
      *   - upload:
+     *   - credentials
      */
     $scope.setState = function(state) {
       $scope.state = state;
+    };
+
+    $scope.isUserLogin = function() {
+        if (storage.get('user') === null) {
+            return false;
+        } else {
+            $scope.setState('mark');
+        }
+
+        console.log($scope.state);
+
     };
 
     /**
@@ -118,6 +130,8 @@ angular.module('publicEducationApp')
     storage.bind($scope, 'center', {defaultValue: Leaflet.getCenter()});
     storage.bind($scope, 'text');
     storage.bind($scope, 'markers');
-    storage.bind($scope, 'state', {defaultValue: 'mark'});
+    //storage.bind($scope, 'user');
+    storage.bind($scope, 'state', {defaultValue: 'credentials'});
+    $scope.setState('credentials');
     updateMarker();
   });
