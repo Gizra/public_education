9c6187edf1d65efff0d284fa8f87d72008e52529
Carlos Mantilla <carlos@gizra.com>
2013-10-15 12:47:44 +0300

    realated to issue #41

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('publicEducationApp')
-  .controller('ListMarkersCtrl', function ($scope, Leaflet, storage, Marker, $location) {
+  .controller('ListMarkersCtrl', function ($scope, Leaflet, storage, Marker, $location, $timeout) {
 
     angular.extend($scope, Leaflet.getDefaults());
     storage.bind($scope,'center', {defaultValue: Leaflet.getCenter()});
@@ -13,21 +13,36 @@ angular.module('publicEducationApp')
     }
 
     $scope.markers = {};
-    Marker.gettingMarkers().then(function(data) {
 
-      angular.forEach(data, function(marker, key) {
-        marker.icon = L.divIcon({
-          iconSize: [63, 71],
-          // Set the icon according to the playlist count.
-          html: '<div class="marker-icon">' + marker.playList.length + '</div>',
-          // @todo: angular-leaflet fails without this one.
-          iconAnchor:   [31, 71]
+    // Get markers.
+    var getMarkers = function getMarkers() {
+      Marker.gettingMarkers().then(function(data) {
+
+        data = data.data;
+
+        angular.forEach(data, function(marker, key) {
+          marker.icon = L.divIcon({
+            iconSize: [63, 71],
+            // Set the icon according to the playlist count.
+            html: '<div class="marker-icon">' + marker.playList.length + '</div>',
+            // @todo: angular-leaflet fails without this one.
+            iconAnchor:   [31, 71]
+          });
+
+          $scope.markers[key] = marker;
         });
-
-        $scope.markers[key] = marker;
+        // $scope.markers = data;
       });
-      // $scope.markers = data;
-    });
+    }
+
+    // Refresh markers each minute.
+    var refreshMarker = function lopp() {
+      getMarkers();
+      $timeout(refreshMarker, 60000)
+    };
+
+    // Start request
+    refreshMarker();
 
     angular.forEach(['zoomend','moveend'], function(value) {
       $scope.$on('leafletDirectiveMap.' + value, function() {
