8071418581806e4f0a530b5f3f5f3e4ebedbbe9a
Amitai Burstein <amitai@gizra.com>
2013-10-21 20:21:08 +0300

    #58: Player page enhancements.

--- app/scripts/controllers/playMarker.js
+++ app/scripts/controllers/playMarker.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('publicEducationApp')
-  .controller('PlayMarkerCtrl', function ($scope, $routeParams, $location, storage, Marker, Leaflet, Phonegap) {
+  .controller('PlayMarkerCtrl', function ($scope, $routeParams, $location, storage, Marker, Leaflet, Phonegap, $window) {
 
     $scope.venueId = $routeParams.venueId;
-      angular.extend($scope, {
-        selectedMarker: {
-          playList: []
-        }
+    angular.extend($scope, {
+      selectedMarker: {
+        playList: [],
+        currentRecord: null
+      }
     });
 
     // Default values, that will be populated once the markers are fetched.
@@ -16,8 +17,19 @@ angular.module('publicEducationApp')
     $scope.playList = [];
     $scope.currentTrack = 0;
 
+    // Default values of a user.
+    $scope.user = {
+      name: 'Anonymous'
+    };
+
+    // Default values edit mode ng-class.
+    $scope.classPlayerMode = 'playlist-info bottom-bar';
+    $scope.editMode = false;
+    $scope.actualPage = $location.absUrl();
+
+
     Marker.gettingMarkers().then(function(data) {
-      $scope.markers = data.data;
+      $scope.markers = data;
 
       if (!$scope.markers[$scope.venueId]) {
         // Redirect to homepage on wrong venue ID.
@@ -34,9 +46,12 @@ angular.module('publicEducationApp')
         lat: $scope.selectedMarker.lat,
         lng: $scope.selectedMarker.lng,
         zoom: 16
-      }
+      };
 
       $scope.$watch('currentTrack', function(track) {
+        // Populate info of current record in the scope.
+        $scope.selectedMarker.currentRecord = $scope.selectedMarker.playList[track];
+
         if (track <= $scope.selectedMarker.playList.length) {
           $scope.playItem($scope.selectedMarker.playList[track].src);
         }
@@ -56,7 +71,7 @@ angular.module('publicEducationApp')
         });
       });
       mediaPlayer.play();
-    }
+    };
 
     angular.extend($scope, Leaflet.getDefaults());
 
@@ -73,5 +88,34 @@ angular.module('publicEducationApp')
       args.leafletEvent.target.keyboard._enabled = false;
 
     });
+
+    /**
+     * Toggle between edit and play mode.
+     */
+    $scope.toggleEditMode = function() {
+      $scope.editMode = !$scope.editMode;
+      $scope.classPlayerMode = (!$scope.editMode) ? 'playlist-info bottom-bar' : 'playlist-info edit-mode';
+    };
+
+    /**
+     * Share link to twitter, facebook, email.
+     */
+    $scope.shareLink = function(method) {
+      var url;
+      var text = $scope.selectedMarker.currentRecord.text + '-' + encodeURIComponent($location.absUrl());
+
+      if (method === 'twitter') {
+        url = 'https://twitter.com/share?text='+text;
+      }
+      else if (method === 'facebook') {
+        url = 'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=' + encodeURIComponent($location.absUrl()) + '&p[title]=Public%20Education&p[summary]=' + text;
+      }
+      else if (method === 'email') {
+        url = 'mailto:?body=' + text + ' - ' + encodeURIComponent($location.absUrl());
+      }
+
+      $window.open(url, method, 'width=626,height=445');
+    };
+
 
   });
