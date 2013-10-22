d48b20a749d911fa0060a354c708f3a8e883804a
Carlos Mantilla <carlos@gizra.com>
2013-10-22 18:29:08 +0300

    - Playing and replaying with PhoneGap - Modify grunt example file to add html directive inside the directives folder. And htmlmin could find it.

--- app/scripts/directives/audioPlayer/audioPlayer.js
+++ app/scripts/directives/audioPlayer/audioPlayer.js
@@ -7,43 +7,45 @@ angular.module('publicEducationApp')
       restrict: 'E',
       scope: {
         playList: '=playList',
-        currentRecord: '=currentRecord'
+        currentRecord: '=currentRecord',
+        list: '='
       },
       link: function postLink(scope) {
 
         scope.isPhoneGap = Phonegap.isMobile.any();
-        scope.currentTrack = 0;
 
-        //alert(Phonegap.isMobile.any());
-        console.log('isMobile:', Phonegap.isMobile.any());
-
+        /**
+         * Play a previous item in HTML5.
+         */
         scope.previous = function() {
           if (scope.currentTrack > 0) {
             --scope.currentTrack;
           }
         };
 
+        /**
+         * Play a next item in HTML5.
+         */
         scope.next = function() {
           if (scope.currentTrack < scope.playList.length - 1) {
             ++scope.currentTrack;
           }
         };
 
+        /**
+         * Play an item in HTML5.
+         */
         scope.playPauseHtml5 = function() {
           scope.playerControl.playPause();
         };
 
         /**
          * Play an item in PhoneGap devices.
-         *
          */
-        scope.playPhoneGap = function() {
+        scope.playPhoneGap = function(src) {
+          console.log('src:', src);
+          scope.mediaPlayer = Phonegap.getMedia(src, function onSuccess() {
 
-          scope.mediaPlayer = Phonegap.getMedia(function onSuccess() {
-            // If play was successful, skip to the next track.
-            scope.$apply(function () {
-              ++scope.currentTrack;
-            });
           });
           scope.mediaPlayer.play();
         };
@@ -74,13 +76,17 @@ angular.module('publicEducationApp')
 
         scope.$watch('currentTrack', function(track, oldTrack) {
           // Populate info of current record in the scope.
-          if (!scope.playList.length) {
+          if (!scope.playList.length, track) {
+            console.log(scope.playList);
             return;
           }
 
+          console.log('track:', track, 'playList:', scope.playList[track]);
           scope.currentRecord = scope.playList[track];
 
+
           if (scope.isPhoneGap) {
+            console.log('scope.currentRecord:', scope.currentRecord);
             scope.playPhoneGap(scope.playList[track].src);
           }
           else if (oldTrack > 0) {
@@ -97,9 +103,25 @@ angular.module('publicEducationApp')
 
 
         scope.$watch('playerControl.currentTrack', function(currentTrack) {
-          // Change current track by the HTML5 <audio> tag.
-          scope.currentTrack = currentTrack - 1;
+          if (!scope.isPhoneGap) {
+            // Change current track by the HTML5 <audio> tag.
+            scope.currentTrack = currentTrack - 1;
+          }
+          // console.log('playControl');
         });
+
+        /**
+         * On playlist updated
+         */
+        scope.$watch('list', function( newPlaylist, oldPlaylist ) {
+
+          if (angular.isDefined(newPlaylist)) {
+            // If exist playlist init current track
+            scope.currentTrack = 0;
+          }
+
+        });
+
       }
     };
   });
