8ef35ce6ec1c1f346c1a661ebc61a5e87322c3ec
Amitaibu <amitai@gizra.com>
2013-10-28 17:09:48 +0200

    #82: Wrap geolocation for devices.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,30 @@
 'use strict';
 
 angular.module('publicEducationApp')
-  .controller('ListMarkersCtrl', function ($scope, $window, Leaflet, storage, Marker, $location, $timeout, IS_MOBILE) {
+  .controller('ListMarkersCtrl', function ($scope, $window, Leaflet, storage, Marker, $location, $timeout, Phonegap, IS_MOBILE) {
 
     angular.extend($scope, Leaflet.getDefaults());
     storage.bind($scope,'center', {defaultValue: Leaflet.getCenter()});
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        $scope.center.lat = position.coords.latitude;
-        $scope.center.lng = position.coords.longitude;
-      });
+
+
+    $scope.onDeviceReady = function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          $scope.center.lat = position.coords.latitude;
+          $scope.center.lng = position.coords.longitude;
+        });
+      }
+    };
+
+    if (Phonegap.isMobile.any()) {
+      // Devices.
+      document.addEventListener('deviceready', $scope.onDeviceReady, false);
     }
+    else {
+      // Web.
+      $scope.onDeviceReady();
+    }
+
 
     $scope.markers = {};
 
