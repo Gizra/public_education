29c47d9c72181f69d87d1319f7b779ca4027f4c5
Carlos Mantilla <carlos@gizra.com>
2013-10-29 18:01:47 +0200

    Added Dummy S

--- app/scripts/controllers/playMarker.js
+++ app/scripts/controllers/playMarker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('publicEducationApp')
-  .controller('PlayMarkerCtrl', function ($scope, $routeParams, $location, storage, Marker, Leaflet, Phonegap, $window) {
+  .controller('PlayMarkerCtrl', function ($scope, $routeParams, $location, storage, Marker, Leaflet, Phonegap, $window, DUMMY_WAV_FILE) {
 
     $scope.venueId = $routeParams.venueId;
     angular.extend($scope, {
@@ -34,7 +34,14 @@ angular.module('publicEducationApp')
         $location.path('/');
       }
 
+      if (!Phonegap.isMobile.any()) {
+        // Set dummy sound for the first sound of the playList to mock the web, on cached new markers.
+        $scope.markers[$scope.venueId].playList[0].src = DUMMY_WAV_FILE;
+      }
       $scope.selectedMarker = $scope.markers[$scope.venueId];
+
+      console.log($scope.selectedMarker.playList[0].src);
+
 
       // Needed to fill the playList of the component angular-audio-player.
       angular.forEach($scope.selectedMarker.playList, function(value) {
