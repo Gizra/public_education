608994b52af8e48755bb4677bc0ca3c15722046b
Carlos Mantilla <carlos@gizra.com>
2013-11-05 16:02:26 +0200

    Prepare local web test server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
+        hostname: '10.0.0.133'
       },
       livereload: {
         options: {
@@ -115,7 +115,7 @@ module.exports = function (grunt) {
     },
     open: {
       server: {
-        url: 'http://localhost:<%= connect.options.port %>'
+        url: 'http://10.0.0.133:<%= connect.options.port %>'
       }
     },
     clean: {
