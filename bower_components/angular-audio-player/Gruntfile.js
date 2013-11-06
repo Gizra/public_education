f02e75d1c2f1d87917fb2e378c9273be84e6135f
Amitai Burstein <amitai@gizra.com>
2013-11-06 16:12:58 +0200

    Updates

--- bower_components/angular-audio-player/Gruntfile.js
+++ bower_components/angular-audio-player/Gruntfile.js
@@ -1,6 +1,7 @@
+var hljs = require('highlight.js');
+
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-connect');
@@ -8,12 +9,16 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-git-describe');
+  // docs grunt-requirements
+  grunt.loadNpmTasks('grunt-html2js');
+  grunt.loadNpmTasks('grunt-md2html');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> v<%=grunt.option("gitRevision") %> | date: <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%=pkg.name %> v<%=pkg.version %> | date: <%=grunt.template.today("dd-mm-yyyy") %> */\n'
       },
       dist: {
         src: 'dist/angular-audio-player.js',
@@ -21,57 +26,11 @@ module.exports = function (grunt) {
       }
     },
     jshint: {
-      files: ['Gruntfile.js', 'src/*.js', 'src/*.spec.js'],
       options: {
-        // options here to override JSHint defaults
-        // Settings
-        'passfail'      : false,  // Stop on first error.
-        'maxerr'        : 10000,    // Maximum error before stopping.
-
-        // Development.
-        'debug'         : false,  // Allow debugger statements e.g. browser breakpoints.
-        'devel'         : false,   // Allow developments statements e.g. `console.log();`.
-
-
-        // ECMAScript 5.
-        'strict'        : false,  // Require `use strict` pragma  in every file.
-        'globalstrict'  : false,  // Allow global 'use strict' (also enables 'strict').
-
-
-        // The Good Parts.
-        'asi'           : false,  // Tolerate Automatic Semicolon Insertion (no semicolons).
-        'laxbreak'      : true,   // Tolerate unsafe line breaks e.g. `return [\n] x` without semicolons.
-        'bitwise'       : false,  // Prohibit bitwise operators (&, |, ^, etc.).
-        'boss'          : false,  // Tolerate assignments inside if, for & while. Usually conditions & loops are for comparison, not assignments.
-        'curly'         : true,   // Require {} for every new block or scope.
-        'eqeqeq'        : true,   // Require triple equals i.e. `===`.
-        'eqnull'        : false,  // Tolerate use of `== null`.
-        'evil'          : false,  // Tolerate use of `eval`.
-        'expr'          : false,  // Tolerate `ExpressionStatement` as Programs.
-        'forin'         : false,  // Tolerate `for in` loops without `hasOwnPrototype`.
-        'immed'         : true,   // Require immediate invocations to be wrapped in parens e.g. `( function(){}() );`
-        'latedef'       : true,   // Prohibit variable use before definition.
-        'loopfunc'      : false,  // Allow functions to be defined within loops.
-        'noarg'         : true,   // Prohibit use of `arguments.caller` and `arguments.callee`.
-        'regexp'        : true,   // Prohibit `.` and `[^...]` in regular expressions.
-        'regexdash'     : false,  // Tolerate unescaped last dash i.e. `[-...]`.
-        'scripturl'     : true,   // Tolerate script-targeted URLs.
-        'shadow'        : false,  // Allows re-define variables later in code e.g. `var x=1; x=2;`.
-        'supernew'      : false,  // Tolerate `new function () { ... };` and `new Object;`.
-        'undef'         : false,  // Require all non-global variables be declared before they are used.
-
-        // Personal styling preferences.
-        'newcap'        : true,   // Require capitalization of all constructor functions e.g. `new F()`.
-        'noempty'       : true,   // Prohibit use of empty blocks.
-        'nonew'         : true,   // Prohibit use of constructors for side-effects.
-        'nomen'         : false,  // Prohibit use of initial or trailing underbars in names.
-        'onevar'        : false,  // Allow only one `var` statement per function.
-        'plusplus'      : false,  // Prohibit use of `++` & `--`.
-        'sub'           : false,  // Tolerate all forms of subscript notation besides dot notation e.g. `dict['key']` instead of `dict.key`.
-        'trailing'      : true,   // Prohibit trailing whitespaces.
-        'white'         : true,   // Check against strict whitespace and indentation rules.
-        'indent'        : 2       // Specify indentation spacing
-      }
+        jshintrc: '.jshintrc'
+      },
+      source: ['Gruntfile.js', 'src/*.js'],
+      docs: ['docs/*.js', 'docs/examples/*.js']
     },
     karma: {
       unit: {
@@ -86,6 +45,21 @@ module.exports = function (grunt) {
       source: {
         src: ['src/directive.js', 'src/helpers.js'],
         dest: 'dist/angular-audio-player.js'
+      },
+      'docs-app': {
+        src: [
+          'docs/docs.js',
+          'docs/examples/*.js'
+        ],
+        dest: 'docs/app.js'
+      },
+      'docs-libs': {
+        src: [
+          'libs/angular/angular.js',
+          'libs/angular-route/angular-route.js',
+          'libs/angular-animate/angular-animate.js',
+        ],
+        dest: 'docs/libs.js'
       }
     },
     watch: {
@@ -95,17 +69,74 @@ module.exports = function (grunt) {
         },
         files: ['src/directive.js', 'src/helpers.js'],
         tasks: ['concat', 'saveRevision', 'uglify']
+      },
+      'docs-app': {
+        files: [
+          'docs/docs.js',
+          'docs/examples/*.js'
+        ],
+        tasks: ['concat:docs-app']
+      },
+      'docs-tpl': {
+        files: [
+          'docs/**/*.tpl.html',
+          'docs/*.tpl.html'
+        ],
+        tasks: ['html2js:docs']
+      },
+      'docs-md': {
+        options: {
+          atBegin: true
+        },
+        files: [
+          'docs/*.md',
+          'docs/examples/*.md'
+        ],
+        tasks: ['md2html', 'html2js:docs']
       }
     },
     connect: {
-      examples: {
+      docs: {
         options: {
           port: 8181,
-          base: 'examples/',
+          base: 'docs/',
           hostname: '*',
           debug: true
         }
       }
+    },
+    html2js: {
+      docs: {
+        options: {
+          base: 'docs'
+        },
+        src: [
+          'docs/**/*.tpl.html',
+          'docs/*.tpl.html'
+        ],
+        dest: 'docs/templates-docs.js'
+      }
+    },
+    md2html: {
+      docs: {
+        files: [
+          { src: ['docs/*.md'], dest: '', ext: '.md.tpl.html', expand: true },
+          { src: ['docs/examples/*.md'], dest: '', ext: '.md.tpl.html', expand: true }
+        ],
+        options: {
+          markedOptions: {
+            highlight: function (code, lang) {
+              return hljs.highlightAuto(code).value;
+            }
+          }
+        }
+      }
+    },
+    clean: {
+      'docs-libs': ['docs/libs.js'],
+      'docs-app': ['docs/app.js'],
+      'docs-tpl': ['docs/templates-docs.js'],
+      'docs-md': ['docs/*.md.tpl.html', 'docs/examples/*.md.tpl.html']
     }
   });
 
@@ -119,5 +150,6 @@ module.exports = function (grunt) {
   grunt.registerTask('test', ['jshint', 'karma']);
   grunt.registerTask('build', ['jshint', 'saveRevision', 'concat', 'uglify']);
   grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('docs', ['clean', 'jshint:docs', 'concat:docs-libs', 'connect:docs', 'watch']);
 
 };
