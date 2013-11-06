3de3f6166118c2cdb2c5395d76f6446193668077
Carlos Mantilla <carlos@gizra.com>
2013-11-06 12:38:19 +0200

    Clean code

--- app/scripts/directives/addMarker/addMarker.js
+++ app/scripts/directives/addMarker/addMarker.js
@@ -8,14 +8,13 @@ angular.module('publicEducationApp')
       scope:true,
       controller: function($scope) {
 
+        // Set the store url in each device.
+        // It's needed set the specific application url info in the config.json file.
         if (!IS_MOBILE && Phonegap.isMobile.iOS()) {
-          // @todo: Need to replace with the name an idi of the application publish in the
-          // iTunes Store of Public Education.
-
-          $scope.url = 'https://itunes.apple.com/app/' + URL_STORE.iOS;
+          $scope.url = URL_STORE.iOS;
         }
         else if (Phonegap.isMobile.Android()) {
-          $scope.url = 'https://play.google.com/store/apps/details?id=' + URL_STORE.android;
+          $scope.url = URL_STORE.android;
         }
         else {
           $scope.url = '#/add-marker';
