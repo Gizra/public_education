f17e8badb50f8361bf6170da1fe5841ecea52faf
Amitaibu <amitai@gizra.com>
2013-11-07 09:21:10 +0200

    Updates

---
+++ scripts/49465b3b.config.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('config', []).constant('ENV', 'production').constant('FOURSQUARE', {
+  'id': 'IZPZNXG2P5A3H4EBIFFEMRAIBMXCNGM1ZZ2QUPN2D1WNKNGX',
+  'secret': 'MZ0CZ4Z0PMAEAGKDPJ5DO0BI2M1FAMJXRGWMCWNCPC5MQ02U'
+}).constant('OAUTHIO', { 'id': 'nCYMyRVLEfy-4Sk_TPQCaey4Hhk' }).constant('RECORD_FLAGS', ['koc']).constant('IS_MOBILE', false).constant('BACKEND_URL', 'http://public-education.herokuapp.com').constant('DUMMY_WAV_FILE', 'http://www.wavsource.com/snds_2013-10-27_4075717104816583/people/men/check_out_whats_new.wav').constant('URL_STORE', {
+  'iOS': 'https://itunes.apple.com/app/chrome/id535886823?mt=8',
+  'android': 'https://play.google.com/store/apps/details?id=com.android.chrome'
+});
+;
