02340840b7a5a4e933691756168ee40070a515cc
Amitaibu <amitai@gizra.com>
2013-11-07 15:06:30 +0200

    Update.

--- scripts/ce5f618b.config.js
+++ scripts/ce5f618b.config.js
@@ -5,5 +5,5 @@ angular.module('config', []).constant('ENV', 'production').constant('FOURSQUARE'
 }).constant('OAUTHIO', { 'id': 'nCYMyRVLEfy-4Sk_TPQCaey4Hhk' }).constant('RECORD_FLAGS', ['koc']).constant('IS_MOBILE', false).constant('BACKEND_URL', 'http://public-education.herokuapp.com').constant('DUMMY_WAV_FILE', 'http://www.wavsource.com/snds_2013-10-27_4075717104816583/people/men/check_out_whats_new.wav').constant('URL_STORE', {
   'iOS': 'https://itunes.apple.com/app/chrome/id535886823?mt=8',
   'android': 'https://play.google.com/store/apps/details?id=com.android.chrome'
-});
+}).constant('URHERE_ICON', 'https://raw.github.com/Gizra/public_education/master/app/images/urhere@2x.png').constant('WEB_URL', 'http://pe.mushon.com');
 ;
